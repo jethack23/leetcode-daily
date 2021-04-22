d6833dd6d25806b9ac1be104d4d939d1bd1757e8
jethack <jethack23@gmail.com>
2021-04-22 11:24:34 +0900

    Update 210419.cpp

--- 2021-04/210419.cpp
+++ 2021-04/210419.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned long long int> mem(target + 1, 0);
+        vector<unsigned int> mem(target + 1, 0);
         mem[0] = 1;
         for (int n = 1; n <= target; n++) {
             for (auto j : nums) {
