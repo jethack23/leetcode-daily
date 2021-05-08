0db3c7899aea922013be32d45a8495b0f55e5772
jethack <jethack23@gmail.com>
2021-05-08 13:39:13 +0900

    210507 less memory

--- 2021-05/210507.py
+++ 2021-05/210507.py
@@ -2,11 +2,13 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
-        mem = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m):
+        mem = [[0] * (n + 1) for _ in range(2)]
+        i = 0
+        for k in range(m):
+            i ^= 1
             for j in range(n):
-                if word1[i] == word2[j]:
+                if word1[k] == word2[j]:
                     mem[i][j] = 1 + mem[i - 1][j - 1]
                 else:
                     mem[i][j] = max(mem[i - 1][j], mem[i][j - 1])
-        return m + n - 2 * mem[m - 1][n - 1]
+        return m + n - 2 * mem[i][n - 1]
