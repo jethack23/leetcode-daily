2ebcaef8edef239f52fa1a08d50ac0890ca12662
jethack <jethack23@gmail.com>
2021-05-10 18:50:59 +0900

    limit range more

--- 2021-05/210510.py
+++ 2021-05/210510.py
@@ -3,7 +3,7 @@ class Solution:
         if n <= 2:
             return 0
         mem = [0, 0] + [1] * (n - 2)
-        for i in range(2, n // 2 + 1):
+        for i in range(2, int(sqrt(n)) + 1):
             if mem[i] != 0:
-                mem[i * i : n : i] = [0] * (max(0, n - 1 - i * i + i) // i)
+                mem[i * i : n : i] = [0] * ((n - 1 - i * i + i) // i)
         return sum(mem)
